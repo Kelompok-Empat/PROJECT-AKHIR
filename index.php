4b4399e0510fbeb9819e653c188bcba9724af999
ajiarindazahra <ajiarindazahra93561@gmail.com>
2023-05-04 18:20:41 +0800

    baikin css nya login sm footer

--- index.php
+++ index.php
@@ -1,41 +1,66 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Contoh 3 Box dengan Bootstrap</title>
-    
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Option</title>
+	
+	    
 	<!-- Load Bootstrap CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+	<style>
+	.card:nth-child(1) {
+		background-color: #2E5F7D;
+		color: #fff;
+	}
+	.card:nth-child(2) {
+		background-color: #8CB27F;
+		color: #fff;
+	}
+	.card:nth-child(3) {
+		background-color: #EAE7EE;
+		color: #000;
+	}
+	</style>
+
 </head>
 <body>
-	<div class="container mt-5">
+	
+<div class="container mt-5">
 		<div class="row">
+
 			<div class="col-md-4">
 				<div class="card">
 					<div class="card-body">
-						<h5 class="card-title">Box 1</h5>
-						<p class="card-text">Isi Box 1</p>
-						<a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+						<h5 class="card-title">Member</h5>
+						<p class="card-text">Login untuk Member</p>
+						<a href="loginmember.php" class="btn btn-primary">Lihat Selengkapnya</a>
 					</div>
 				</div>
 			</div>
+
 			<div class="col-md-4">
 				<div class="card">
 					<div class="card-body">
-						<h5 class="card-title">Box 2</h5>
-						<p class="card-text">Isi Box 2</p>
-						<a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+						<h5 class="card-title">Admin</h5>
+						<p class="card-text">Login untuk Admin</p>
+						<a href="loginadmin.php" class="btn btn-primary">Lihat Selengkapnya</a>
 					</div>
 				</div>
 			</div>
+
 			<div class="col-md-4">
 				<div class="card">
 					<div class="card-body">
-						<h5 class="card-title">Box 3</h5>
-						<p class="card-text">Isi Box 3</p>
-						<a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+						<h5 class="card-title">Staff</h5>
+						<p class="card-text">Login untuk Staff</p>
+						<a href="loginstaff.php" class="btn btn-primary">Lihat Selengkapnya</a>
 					</div>
 				</div>
 			</div>
+
 		</div>
 	</div>
 
