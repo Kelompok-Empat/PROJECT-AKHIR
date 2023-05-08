fe35e9236c63c9dce2e45d43d09107b492179090
ajiarindazahra <ajiarindazahra93561@gmail.com>
2023-05-09 07:54:20 +0800

    ganti index

--- index.php
+++ index.php
@@ -5,71 +5,113 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Portal | Mountain Lodge</title>
-
-	<!-- Load Bootstrap CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-
-	<link rel="stylesheet" href="css/portal.css">
+	<title>Mountain Lodge</title>
 
 	<style>
-		.card:nth-child(1) {
-			background-color: #2E5F7D;
-			color: #fff;
+		body {
+			font-family: Arial, sans-serif;
+			background-color: #f5f5f5;
+			margin: 0;
+			padding: 0;
 		}
 
-		.card:nth-child(2) {
-			background-color: #8CB27F;
+		header {
+			background-color: #333;
 			color: #fff;
+			padding: 20px;
+			text-align: center;
 		}
 
-		.card:nth-child(3) {
-			background-color: #EAE7EE;
-			color: #000;
+		h1 {
+			margin: 0;
+			font-size: 36px;
+			font-weight: normal;
+		}
+
+		.container {
+			max-width: 800px;
+			margin: 20px auto;
+			padding: 20px;
+			background-color: #fff;
+			box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+		}
+
+		p {
+			margin-bottom: 20px;
+			line-height: 1.5;
+		}
+
+		ul {
+			margin: 0;
+			padding: 0;
+			list-style: none;
+		}
+
+		ul li {
+			margin-bottom: 10px;
+		}
+
+		ul li:last-child {
+			margin-bottom: 0;
+		}
+
+		ul li:before {
+			content: "\2022";
+			margin-right: 10px;
+			color: #333;
+		}
+
+		.btn {
+			display: inline-block;
+			padding: 10px 20px;
+			background-color: #333;
+			color: #fff;
+			text-decoration: none;
+			border-radius: 5px;
+		}
+
+		.btn:hover {
+			background-color: #444;
 		}
 	</style>
-
 </head>
 
+
 <body>
-	<div class="container mt-5">
-		<div class="row">
-			<div class="col-md-4">
-				<div class="card">
-					<div class="card-body">
-						<h5 class="card-title">Member</h5>
-						<p class="card-text">Login untuk Member</p>
-						<a href="portal/loginmember.php" class="btn btn-primary">Masuk Sekarang!</a>
-					</div>
-				</div>
-			</div>
-
-			<div class="col-md-4">
-				<div class="card">
-					<div class="card-body">
-						<h5 class="card-title">Admin</h5>
-						<p class="card-text">Login untuk Admin</p>
-						<a href="portal/loginadmin.php" class="btn btn-primary">Masuk Sekarang!</a>
-					</div>
-				</div>
-			</div>
-
-			<div class="col-md-4">
-				<div class="card">
-					<div class="card-body">
-						<h5 class="card-title">Staff</h5>
-						<p class="card-text">Login untuk Staff</p>
-						<a href="portal/loginstaff.php" class="btn btn-primary">Masuk Sekarang!</a>
-					</div>
-				</div>
-			</div>
+	
+  
+	<header>
+		<div class = "logo">
+			<a href="berandamember.php"><img src="../img/1.png" width="20%"></a>
 		</div>
-	</div>
+	</header>
 
-	<!-- Load Bootstrap JS -->
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<nav style="text-align: right;">
+			
+		<a href="loginlanjutan.php">Login Untuk Reservasi</a>
+
+	</nav>
+
+	<main>
+
+		<div class="container">
+			<p>Mountain Lodge adalah sebuah hotel mewah yang terletak di kaki Gunung Everest. Kami menawarkan pengalaman menginap yang tak terlupakan bagi para tamu kami, dengan pemandangan alam yang indah dan fasilitas yang lengkap.</p>
+			<p>Kami menyediakan berbagai jenis kamar, mulai dari kamar standar hingga suite mewah dengan pemandangan gunung yang spektakuler. Selain itu, kami juga memiliki restoran yang menyajikan berbagai masakan lokal dan internasional, serta spa untuk relaksasi dan perawatan tubuh.</p>
+			<p>Kami selalu berusaha memberikan pelayanan terbaik bagi para tamu kami, dengan staf yang ramah dan profesional siap membantu setiap kebutuhan Anda selama menginap di Mountain Lodge.</p>
+			<h2>Fasilitas dan Layanan</h2>
+			<ul>
+				<li>Kolam Renang</li>
+				<li>Restoran</li>
+				<li>Spa</li>
+				<li>Pusat Kebugaran</li>
+				<li>Ruang Rapat</li>
+				<li>Pelayanan Kamar 24 Jam</li>
+				<li>Laundry dan Dry Cleaning</li>
+				<li>Parkir Gratis</li>
+				<li>Wi-Fi Gratis</li>
+			</ul>
+			<a href="#" class="btn">Hubungi Kami</a>
+		</div>
+
+	</main>
 </body>
-
-</html>
